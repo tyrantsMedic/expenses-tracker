50c55a9599dde5c86ce571be457d95b783de5bbb
Hamza Nawaz <hamzaofe786@hotmail.com>
2024-09-08 05:30:38 +0500

    added a backend

---
+++ backend/server.js
@@ -0,0 +1,161 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import pg from 'pg'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+const dbName = 'expenses_tracker'
+const tableName = 'transactions'
+const exists = await checkDatabaseExists(dbName)
+
+if (!exists) {
+  await createDatabase(dbName)
+}
+
+const client = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  password: 'lhu@5934',
+  port: 5432,
+  database: dbName
+})
+
+await client.connect()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/getAllTransactions', async (req, res) => {
+  try {
+    const result = await client.query('SELECT * FROM transactions')
+    res.status(200).json(result.rows)
+  } catch (error) {
+    console.error('Error fetching transactions:', error)
+    res.status(500).json({ error: 'An error occurred while fetching transactions' })
+  }
+})
+
+app.post('/api/addTransaction', async (req, res) => {
+  try {
+    const { amount, text } = req.body
+
+    if (!amount || !text) {
+      return res.status(400).json({ error: 'Amount and description are required' })
+    }
+    const result = await client.query(
+      'INSERT INTO transactions (amount, text) VALUES ($1, $2) RETURNING *',
+      [amount, text]
+    )
+    res.status(201).json(result.rows[0])
+  } catch (error) {
+    console.error('Error inserting data:', error)
+    res.status(500).json({ error: 'Database error' })
+  }
+})
+
+app.delete('/api/transaction', async (req, res) => {
+  try {
+    await client.query('DELETE FROM transactions WHERE id = $1', [req.body.id])
+    res.status(201).json('Transaction deleted Successfully!')
+  } catch (error) {
+    console.error('Error inserting data:', error)
+    res.status(500).json({ error: 'Database error' })
+  }
+})
+
+app.delete('/api/all', async (req, res) => {
+  try {
+    await client.query('DELETE FROM transactions')
+    res.status(201).json('All transactions deleted Successfully!')
+  } catch (error) {
+    console.error('Error inserting data:', error)
+    res.status(500).json({ error: 'Database error' })
+  }
+})
+
+app.listen(port, () => {
+  console.log('Port is running on ', port)
+})
+
+function createClient(dbName = 'postgres') {
+  return new pg.Client({
+    user: 'postgres',
+    host: 'localhost',
+    password: 'lhu@5934',
+    port: 5432,
+    database: dbName
+  })
+}
+
+async function checkDatabaseExists(dbName) {
+  const client = createClient()
+
+  try {
+    await client.connect()
+
+    const res = await client.query(`SELECT 1 FROM pg_database WHERE datname = $1`, [dbName])
+
+    if (res.rowCount > 0) {
+      console.log(`Database "${dbName}" already exists.`)
+      await client.end()
+      const secondClient = createClient(dbName)
+      await secondClient.connect()
+      await checkTableExists(tableName, secondClient)
+      return true
+    } else {
+      console.log(`Database "${dbName}" does not exist.`)
+      await client.end()
+      return false
+    }
+  } catch (error) {
+    console.error('Error checking database existence:', error)
+    return false
+  }
+}
+
+async function createDatabase(dbName) {
+  const client = createClient()
+  try {
+    await client.connect()
+    await client.query(`CREATE DATABASE ${dbName}`)
+    console.log(`Database "${dbName}" created successfully.`)
+    await checkTableExists(tableName, client)
+  } catch (error) {
+    console.error('Error creating database:', error)
+  }
+}
+
+async function checkTableExists(tableName, client) {
+  try {
+    const res = await client.query(
+      `SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = $1`,
+      [tableName]
+    )
+
+    if (res.rowCount > 0) {
+      console.log(`Table "${tableName}" exists.`)
+      return true
+    } else {
+      console.log(`Table "${tableName}" does not exist.`)
+      createTransactionTable(client)
+      return false
+    }
+  } catch (error) {
+    console.error('Error checking table existence:', error)
+    return false
+  }
+}
+
+async function createTransactionTable(client) {
+  try {
+    await client.query(
+      `CREATE TABLE IF NOT EXISTS transactions(id SERIAL PRIMARY KEY, text TEXT, amount VARCHAR(10))`
+    )
+    console.log('Table "transactions" created successfully (if it did not already exist).')
+  } catch (error) {
+    console.error('Error creating table:', error)
+  } finally {
+    await client.end()
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@ import TotalBalance from './components/TotalBalance.vue'
 import IncomeExpense from './components/IncomeExpense.vue'
 import ExpenseHistory from './components/ExpenseHistory.vue'
 import TransactionForm from './components/TransactionForm.vue'
+import axios from 'axios'
 
 export default {
   components: {
@@ -35,19 +36,14 @@ export default {
       colorChangedToRed: false
     }
   },
-  mounted() {
-    if (localStorage.getItem('transactions') == null) {
-      localStorage.setItem('transactions', [''])
-    }
-    if (localStorage.getItem('transactions') != '') {
-      this.transactions = JSON.parse(localStorage.getItem('transactions'))
-    }
+  async mounted() {
+    await this.refresh()
   },
   computed: {
     income() {
       return this.transactions
         .reduce((total, transaction) => {
-          if (transaction.amount > 0) total += transaction.amount
+          if (transaction.amount > 0) total += Number(transaction.amount)
           return total
         }, 0)
         .toFixed(2)
@@ -55,38 +51,57 @@ export default {
     expense() {
       return this.transactions
         .reduce((total, transaction) => {
-          if (transaction.amount < 0) total += transaction.amount
+          if (transaction.amount < 0) total += Number(transaction.amount)
           return total
         }, 0)
         .toFixed(2)
     },
     total() {
       return this.transactions
-        .reduce((total, transaction) => (total += transaction.amount), 0)
+        .reduce((total, transaction) => (total += Number(transaction.amount)), 0)
         .toFixed(2)
     }
   },
   methods: {
-    onAddTransaction(data) {
-      this.transactions.push({
-        id: this.generateUniqueID(),
-        ...data
-      })
-      localStorage.setItem('transactions', JSON.stringify(this.transactions))
+    async onAddTransaction(data) {
+      try {
+        await axios.post('http://localhost:3000/api/addTransaction', data)
+        await this.refresh()
+      } catch (e) {
+        console.error(e)
+      }
+      // localStorage.setItem('transactions', JSON.stringify(this.transactions))
     },
-    generateUniqueID() {
-      return Math.floor(Math.random() * 10000)
-    },
-    onDeleteTransaction(id) {
-      this.transactions = this.transactions.filter((transaction) => transaction.id != id)
-      localStorage.setItem('transactions', JSON.stringify(this.transactions))
+    async onDeleteTransaction(id) {
+      try {
+        await axios.delete('http://localhost:3000/api/transaction', { data: { id: id } })
+        await this.refresh()
+      } catch (e) {
+        console.error(e)
+      }
+      // this.transactions = this.transactions.filter((transaction) => transaction.id != id)
+      // localStorage.setItem('transactions', JSON.stringify(this.transactions))
     },
     async onResetTransactions() {
-      await localStorage.setItem('transactions', [''])
-      this.transactions = []
+      try {
+        await axios.delete('http://localhost:3000/api/all')
+        await this.refresh()
+      } catch (e) {
+        console.error(e)
+      }
+      // await localStorage.setItem('transactions', [''])
+      // this.transactions = []
     },
     onIsTextChangedToRed(isTextChangedToRed) {
       this.colorChangedToRed = isTextChangedToRed
+    },
+    async refresh() {
+      try {
+        const response = await axios.get('http://localhost:3000/api/getAllTransactions')
+        this.transactions = response.data
+      } catch (error) {
+        console.error('Error fetching transactions:', error)
+      }
     }
   }
 }
